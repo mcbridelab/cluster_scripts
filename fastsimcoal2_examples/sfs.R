87433fb36258c26441bea3adabae0f0daaed37d1
Noah H. Rose <noahr@della5.princeton.edu>
2020-05-27 11:38:43 -0400

    add commented out transpose option to sfs.R

--- fastsimcoal2_examples/sfs.R
+++ fastsimcoal2_examples/sfs.R
@@ -5,8 +5,10 @@ mat<-scan(args[1])
 print(args)
 ncols<-2*as.numeric(args[2])+1
 nrows<-2*as.numeric(args[3])+1
-flat<-matrix(mat,ncol=ncols,byrow=T)
-print(flat)
+#necessary to round? not sure
+flat<-matrix(round(mat),ncol=ncols,byrow=T)
+#might need to transpose if you are doing more than two populations
+#flat<-t(flat)
 colnames(flat)<-paste('d0_',0:(ncols-1),sep='')
 rownames(flat)<-paste('d1_',0:(nrows-1),sep='')
 write.table(flat,file=args[4],quote=F,sep='\t')
